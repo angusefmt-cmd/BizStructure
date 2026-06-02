93d09434540ef9d622e856bffcc4678fce1f1de3
angusefmt-cmd <angusefmt@gmail.com>
2026-06-02 18:35:35 +0100

    Add files via upload

--- public/js/config.js
+++ public/js/config.js
@@ -4,4 +4,4 @@
 // ============================================================
 
 const SUPABASE_URL = 'https://ecojxhypohzfcpsrumeh.supabase.co';
-const SUPABASE_ANON_KEY = 'sb_publishable_rTDKprs_eVZkr9XMIJ2-XA_t65D1zC0';
+const SUPABASE_ANON_KEY = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImVjb2p4aHlwb2h6ZmNwc3J1bWVoIiwicm9sZSI6ImFub24iLCJpYXQiOjE3ODA0MTA4NzEsImV4cCI6MjA5NTk4Njg3MX0.hcNRhrEf8o03j9VHrUmjm-TzuHl5Txq2NrPNUL0uGSg';
